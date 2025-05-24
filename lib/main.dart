71c7e9a1112789f26e60ab135eea82601a7916f3
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-05-24 12:33:00 +0100

    add Scaffold with AppBar and ElevatedButton to MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter App"),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              // Action when the button is pressed
+              print("Button Pressed!");
+            },
+            child: const Text("Press Me"),
+          ),
+        ),
+      ),
     );
   }
 }
