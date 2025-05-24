871814d8d84613534855522787e8c2ecb7608ece
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-05-24 12:17:19 +0100

    add MyWidget class to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,12 @@ import "package:flutter/material.dart";
 void main() {
   runApp(MyApp());
 }
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
