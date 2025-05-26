6bf235bb71a667ae133ca95ec32c2451338715dc
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-05-26 08:29:48 +0100

    The decoration property with the corresponding value InputDecoration is set it

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,9 @@ class _MyAppState extends State<MyApp> {
                     hintText: "Email"
                   ),
                 ),
-                TextFormField(),
+                TextFormField(
+                  decoration: InputDecoration()
+                ),
               ],
             ),
           ),
