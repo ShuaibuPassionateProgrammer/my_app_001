adb12cf9afb6f444433c4e357e172b3abe456f2a
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-05-26 08:24:28 +0100

    The main child of the Padding of the SafeArea is changed from Container to the Column

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class _MyAppState extends State<MyApp> {
         body: SafeArea(
           child: Padding(
             padding: EdgeInsets.zero,
-            child: Container(),
+            child: Column(),
           ),
         ),
       ),
