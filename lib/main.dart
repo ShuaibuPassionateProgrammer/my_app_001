04c26e54bc3f7b87692171c471671a95ee5bbee2
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-05-26 06:16:49 +0100

    SafeArea is added to the Body

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatefulWidget {
       home: Scaffold(
         drawer: Drawer(),
         appBar: AppBar(),
-        body: SafeArea(),
+        body: const SafeArea(),
       ),
     );
   }
