bd44bc48e791c9f211e8dbe785e2675428500c5c
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-05-26 06:59:32 +0100

    A primarySwatch Proper is added to the ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        
+        primarySwatch: Colors.blue
       ),
       home: Scaffold(
         drawer: Navbar(),
